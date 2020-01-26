b01b699eb74d48ad7c4892d2b13a1f30abf3640f
suvee01 <suvekshakhnl98@gmail.com>
2020-01-26 13:10:57 +0545

    added

--- app/src/main/java/com/example/onlineliquorfinal/DashboardActivity.java
+++ app/src/main/java/com/example/onlineliquorfinal/DashboardActivity.java
@@ -3,13 +3,19 @@ package com.example.onlineliquorfinal;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.onlineliquorfinal.Fragment.DashboardFragment;
 
 public class DashboardActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,9 @@ public class DashboardActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        frameLayout=findViewById(R.id.drawyerlayout);
+        setFragment(new DashboardFragment());
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
@@ -32,4 +41,12 @@ public class DashboardActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
 
     }
+
+    public void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(frameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+
+    }
+
 }
