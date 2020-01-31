4d5ba07ab02d4e7a5be70e5239a48c42b5c5ffb4
suvee01 <suvekshakhnl98@gmail.com>
2020-01-31 12:56:58 +0545

    layout

--- app/src/main/java/com/example/onlineliquorfinal/AccountActivity.java
+++ app/src/main/java/com/example/onlineliquorfinal/AccountActivity.java
@@ -1,14 +1,30 @@
 package com.example.onlineliquorfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.example.onlineliquorfinal.Fragment.AccountFragment;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private FrameLayout accountframe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        accountframe=findViewById(R.id.aframelayout);
+        setFragment(new AccountFragment());
+    }
+
+    private void setFragment(AccountFragment accountFragment) {
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(accountframe.getId(),accountFragment);
+        fragmentTransaction.commit();
     }
 }
